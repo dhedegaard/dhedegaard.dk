4d1adad06de57c87be94ab83cd8ef86bbe3dd315
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-13 18:25:12 +0100

    Slight fixes to head elements.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,12 +8,10 @@ const Layout: React.FC = props => (
     <Helmet title="Dennis Hedegaard" defer={false}>
       <html lang="en" />
       <link rel="icon" href={favicon} />
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width"
-      />
       <meta name="description" content="The website of Dennis Hedegaard" />
+      <meta property="og:title" content="Dennis Hedegaard" />
       <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://dhedegaard.dk/" />
       <link rel="canonical" href="https://dhedegaard.dk/" />
     </Helmet>
     <CssBaseline />
