c2666236e8041688232506f0f7866f2d741db22e
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-13 18:15:35 +0100

    Fix missing width/height on fontawesome icons before js takes over.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Typography, Link } from "@material-ui/core";
 import React from "react";
 import Layout from "../layout";
+import styled from "styled-components";
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  width: 16px;
+  height: 16px;
+`;
 
 const Index: React.FC = () => (
   <Layout>
@@ -18,15 +24,15 @@ const Index: React.FC = () => (
     <Typography paragraph>
       Find me on{" "}
       <Link href="https://github.com/dhedegaard">
-        <FontAwesomeIcon icon={faGithub} /> Github
+        <StyledFontAwesomeIcon icon={faGithub} /> Github
       </Link>
       ,{" "}
       <Link href="https://www.linkedin.com/in/dennis-hedegaard-39a02a22/">
-        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        <StyledFontAwesomeIcon icon={faLinkedin} /> LinkedIn
       </Link>{" "}
       or send me a{" "}
       <Link href="mailto:dennis@dhedegaard.dk">
-        <FontAwesomeIcon icon={faEnvelope} /> mail
+        <StyledFontAwesomeIcon icon={faEnvelope} /> mail
       </Link>
       .
     </Typography>
