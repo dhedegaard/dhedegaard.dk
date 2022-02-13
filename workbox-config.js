d219dc43d1194b81c25961821b9cdaf2ef5fbdf4
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-13 13:01:20 +0100

    try to avoid caching 404

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   globDirectory: "out/",
   globPatterns: ["**/*.{js,html,png,json,txt}"],
+  globIgnores: ["404.html"],
   swDest: "out/sw.js",
   inlineWorkboxRuntime: true,
   skipWaiting: true,
