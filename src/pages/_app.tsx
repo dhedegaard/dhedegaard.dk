e5018356c826981d9d85edf98e787c881a46726b
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-13 12:27:22 +0100

    meta tags and manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,10 +14,18 @@ const Layout: FC<AppProps> = ({ Component, pageProps }) => (
       <title>Dennis Hedegaard</title>
       <link rel="icon" href="/favicon.png" />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="description" content="The website of Dennis Hedegaard" />
+      <meta
+        name="description"
+        content="The personal website of Dennis Hedegaard"
+      />
       <meta property="og:title" content="Dennis Hedegaard" />
-      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Dennis Hedegaard" />
       <meta property="og:url" content="https://www.dhedegaard.dk/" />
+      <meta
+        property="og:description"
+        content="The personal website of Dennis Hedegaard"
+      />
+      <meta property="og:type" content="website" />
       <link rel="canonical" href="https://www.dhedegaard.dk/" />
       <link rel="apple-touch-icon" href="/favicon.png" />
       <meta name="theme-color" content="#fff" />
