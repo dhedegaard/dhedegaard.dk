c3fb2bfe40dbdcb652b11a18959907d757099445
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-20 11:43:12 +0100

    Added some headers

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  headers: async () => [
+    {
+      key: "X-Frame-Options",
+      value: "SAMEORIGIN",
+    },
+    {
+      key: "X-Content-Type-Options",
+      value: "nosniff",
+    },
+    {
+      key: "Referrer-Policy",
+      value: "origin-when-cross-origin",
+    },
+  ],
 };
