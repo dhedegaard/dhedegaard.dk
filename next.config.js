810f36729aa5b2a0dc3b2831b8f99f43b1b1283d
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-20 12:08:27 +0100

    Permissions-Policy

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ module.exports = {
         { key: "X-Content-Type-Options", value: "nosniff" },
         { key: "Referrer-Policy", value: "origin-when-cross-origin" },
         {
+          key: "Permissions-Policy",
+          value:
+            "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()",
+        },
+        {
           key: "Content-Security-Policy",
           value: csp.join("; "),
         },
