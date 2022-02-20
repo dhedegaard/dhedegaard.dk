097da2644593bb18d386f331992dd44be020148e
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-20 11:45:27 +0100

    fix for headers

--- next.config.js
+++ next.config.js
@@ -9,16 +9,12 @@ module.exports = {
   },
   headers: async () => [
     {
-      key: "X-Frame-Options",
-      value: "SAMEORIGIN",
-    },
-    {
-      key: "X-Content-Type-Options",
-      value: "nosniff",
-    },
-    {
-      key: "Referrer-Policy",
-      value: "origin-when-cross-origin",
+      source: "/:path*",
+      headers: [
+        { key: "X-Frame-Options", value: "SAMEORIGIN" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+      ],
     },
   ],
 };
