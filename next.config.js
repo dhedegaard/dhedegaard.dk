383d47a9ceceb05cfff61ce9b30a2a19306b547d
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-20 12:06:24 +0100

    added CSP header

--- next.config.js
+++ next.config.js
@@ -14,7 +14,19 @@ module.exports = {
         { key: "X-Frame-Options", value: "SAMEORIGIN" },
         { key: "X-Content-Type-Options", value: "nosniff" },
         { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+        {
+          key: "Content-Security-Policy",
+          value: csp.join("; "),
+        },
       ],
     },
   ],
 };
+
+const csp = [
+  "default-src 'unsafe-inline'",
+  "img-src 'self' https://avatars.githubusercontent.com",
+  "script-src 'self' 'unsafe-eval'",
+  "manifest-src 'self'",
+  "connect-src 'self'",
+];
