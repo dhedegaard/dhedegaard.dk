2faff7bdd153f95b999c058e67827b7e49103993
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-03-05 12:34:56 +0100

    Added some fancy animations.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
@@ -9,7 +10,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Chip from "@mui/material/Chip";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+import Typography, { TypographyProps } from "@mui/material/Typography";
 import Box from "@mui/system/Box";
 import { orderBy, uniqBy } from "lodash";
 import { GetStaticProps } from "next";
@@ -24,17 +25,12 @@ interface Props {
 
 const Index: FC<Props> = ({ repositories, avatarUrl, bio }) => (
   <>
+    <Keyframes />
     <Box display="flex" justifyContent="space-between" gap={2} mt={4} mb={8}>
       <Box flex="auto" display="flex" flexDirection="column" gap={3}>
-        <Typography variant="h3" component="h1">
-          Dennis Hedegaard
-        </Typography>
-        {bio != null && (
-          <Typography variant="subtitle1" component="p">
-            {bio}
-          </Typography>
-        )}
-        <Typography variant="subtitle1" component="p">
+        <Title>Dennis Hedegaard</Title>
+        {bio != null && <Bio>{bio}</Bio>}
+        <FindMe>
           Find me on{" "}
           <Link href="https://github.com/dhedegaard" underline="none">
             <FAIcon icon={faGithub} size="sm" />
@@ -57,14 +53,14 @@ const Index: FC<Props> = ({ repositories, avatarUrl, bio }) => (
             <span>mail</span>
           </Link>
           .
-        </Typography>
+        </FindMe>
       </Box>
       {avatarUrl != null && (
         <Avatar src={avatarUrl} alt="Me" crossOrigin="anonymous" />
       )}
     </Box>
     {repositories != null && repositories.length > 0 && (
-      <>
+      <Repositories>
         <Typography variant="h6" component="h2" sx={{ mb: 2 }}>
           Interresting Github projects
         </Typography>
@@ -73,7 +69,7 @@ const Index: FC<Props> = ({ repositories, avatarUrl, bio }) => (
             <Repo key={repo.id} repo={repo} />
           ))}
         </RepositoriesBox>
-      </>
+      </Repositories>
     )}
   </>
 );
@@ -156,11 +152,103 @@ const Repo: FC<{ repo: GithubRepository }> = ({ repo }) => (
   </RepoPaper>
 );
 
+const Keyframes: FC = () => (
+  <Global
+    styles={css`
+      @keyframes slideInLeft {
+        0% {
+          transform: translateX(-20px);
+          opacity: 0;
+        }
+        50% {
+          transform: translateX(-20px);
+          opacity: 0;
+        }
+        100% {
+          transform: translateX(0);
+          opacity: 1;
+        }
+      }
+    `}
+  />
+);
+
+const Title = styled((props: TypographyProps) => (
+  // @ts-expect-error
+  <Typography component="h1" variant="h3" {...props} />
+))`
+  @keyframes slideTitle {
+    0% {
+      transform: translateY(-20px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  animation: 0.5s ease-out 0s 1 slideTitle;
+`;
+
+const Bio = styled((props: TypographyProps) => (
+  // @ts-expect-error
+  <Typography component="p" variant="subtitle1" {...props} />
+))`
+  @keyframes slideBio {
+    0% {
+      transform: translateX(-20px);
+      opacity: 0;
+    }
+    50% {
+      transform: translateX(-20px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+  animation: 1s ease-out 0s 1 slideBio;
+`;
+
+const FindMe = styled((props: TypographyProps) => (
+  // @ts-expect-error
+  <Typography variant="subtitle1" component="p" {...props} />
+))`
+  @keyframes slideFindMe {
+    0% {
+      transform: translateX(20px);
+      opacity: 0;
+    }
+    66% {
+      transform: translateX(20px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+  animation: 1.5s ease-out 0s 1 slideFindMe;
+`;
+
 const HomepageLink = styled(Link)`
   overflow-x: hidden;
   text-overflow: ellipsis;
 `;
 const Avatar = styled.img`
+  @keyframes slideAvatar {
+    0% {
+      transform: translateY(-20px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+  will-change: transform, opacity;
+  animation: 1s ease-out 0s 1 slideAvatar;
   width: 90px;
   aspect-ratio: 1;
   object-fit: cover;
@@ -171,6 +259,21 @@ const Avatar = styled.img`
   @media (max-width: 768px) {
     width: 60px;
   }
+`;
+
+const Repositories = styled.div`
+  @keyframes slideRepositories {
+    0% {
+      opacity: 0;
+    }
+    75% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  animation: 2s ease-out 0s 1 slideRepositories;
 `;
 
 const FAIcon = styled(FontAwesomeIcon)`
