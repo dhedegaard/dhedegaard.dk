83bbccfaf0516e2f060eba63f403aabf14dc8529
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-02-25 20:45:16 +0100

    whoops, missing return in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 export default function NotFound() {
-  <>
-    <h1 className="text-4xl mb-5">404: Not found</h1>
-    <p>
-      The page does not exist.{" "}
-      <Link className="text-blue-600" href="/">
-        Go to the main page
-      </Link>
-      .
-    </p>
-  </>;
+  return (
+    <>
+      <h1 className="text-4xl mb-5">404: Not found</h1>
+      <p>
+        The page does not exist.{" "}
+        <Link className="text-blue-600" href="/">
+          Go to the main page
+        </Link>
+        .
+      </p>
+    </>
+  );
 }
