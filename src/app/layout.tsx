9bba20cc30abf8c7d142243e6c2c577606fc6b4d
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-05-25 16:33:31 +0200

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,23 @@
+import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "../styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Dennis Hedegaard",
+  description: "The personal website of Dennis Hedegaard",
+  manifest: "/manifest.json",
+  themeColor: "#fff",
+  appleWebApp: {
+    capable: true,
+    title: "Dennis Hedegaard",
+  },
+  openGraph: {
+    type: "website",
+    title: "Dennis Hedegaard",
+    siteName: "Dennis Hedegaard",
+    url: "https://www.dhedegaard.dk/",
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
