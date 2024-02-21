3ea1cc2fe8b1457f71dd8878fa895ffa0bc37c98
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-02-21 21:15:29 +0100

    daisyui light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,77 +1,81 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       animation: {
-        slideTitle: "0.5s ease-out 0s 1 slideTitle",
-        slideAvatar: "0.5s ease-out 0s 1 slideAvatar",
-        slideBio: "0.75s ease-out 0s 1 slideBio",
-        slideFindMe: "1s ease-out 0s 1 slideFindMe",
-        slideRepositories: "1.25s ease-out 0s 1 slideRepositories",
+        slideTitle: '0.5s ease-out 0s 1 slideTitle',
+        slideAvatar: '0.5s ease-out 0s 1 slideAvatar',
+        slideBio: '0.75s ease-out 0s 1 slideBio',
+        slideFindMe: '1s ease-out 0s 1 slideFindMe',
+        slideRepositories: '1.25s ease-out 0s 1 slideRepositories',
       },
       keyframes: {
         slideTitle: {
-          "0%": {
-            transform: "translateY(-20px)",
-            opacity: "0",
+          '0%': {
+            transform: 'translateY(-20px)',
+            opacity: '0',
           },
-          "100%": {
-            transform: "translateY(0)",
-            opacity: "1",
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
           },
         },
         slideAvatar: {
-          "0%": {
-            transform: "translateY(-20px)",
-            opacity: "0",
+          '0%': {
+            transform: 'translateY(-20px)',
+            opacity: '0',
           },
-          "100%": {
-            transform: "translateX(0)",
-            opacity: "1",
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
           },
         },
         slideBio: {
-          "0%": {
-            transform: "translateX(-20px)",
-            opacity: "0",
+          '0%': {
+            transform: 'translateX(-20px)',
+            opacity: '0',
           },
-          "33%": {
-            transform: "translateX(-20px)",
-            opacity: "0",
+          '33%': {
+            transform: 'translateX(-20px)',
+            opacity: '0',
           },
-          "100%": {
-            transform: "translateX(0)",
-            opacity: "1",
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
           },
         },
         slideFindMe: {
-          "0%": {
-            transform: "translateX(20px)",
-            opacity: "0",
+          '0%': {
+            transform: 'translateX(20px)',
+            opacity: '0',
           },
-          "50%": {
-            transform: "translateX(20px)",
-            opacity: "0",
+          '50%': {
+            transform: 'translateX(20px)',
+            opacity: '0',
           },
-          "100%": {
-            transform: "translateX(0)",
-            opacity: "1",
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: '1',
           },
         },
         slideRepositories: {
-          "0%": {
-            opacity: "0",
+          '0%': {
+            opacity: '0',
           },
-          "60%": {
-            opacity: "0",
+          '60%': {
+            opacity: '0',
           },
-          "100%": {
-            opacity: "1",
+          '100%': {
+            opacity: '1',
           },
         },
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+    logs: false,
+  },
+}
