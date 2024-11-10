2aa5edfded5ee67e4b0e76b244d191a333d6ae28
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-11-10 16:25:04 +0100

    ran sentry wizard

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://3e5c43b4cf4d92403a9ab2f22b07e1a0@o59230.ingest.us.sentry.io/4506576747429888',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,15 +1,13 @@
 import * as Sentry from '@sentry/nextjs'
 
-export function register() {
-  Sentry.init({
-    dsn: 'https://3e5c43b4cf4d92403a9ab2f22b07e1a0@o59230.ingest.sentry.io/4506576747429888',
+export async function register() {
+  if (process.env['NEXT_RUNTIME'] === 'nodejs') {
+    await import('../sentry.server.config')
+  }
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-
-    ignoreErrors: [/Suspense Exception/],
-  })
+  if (process.env['NEXT_RUNTIME'] === 'edge') {
+    await import('../sentry.edge.config')
+  }
 }
+
+export const onRequestError = Sentry.captureRequestError
