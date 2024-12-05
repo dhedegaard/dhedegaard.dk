710e6018dd59ca0c918c2632e9d465f991391de5
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-12-05 17:15:08 +0100

    removed the headers stuff, including CSP

--- next.config.ts
+++ next.config.ts
@@ -20,37 +20,7 @@ const config: NextConfig = {
       },
     ],
   },
-  headers: () =>
-    Promise.resolve([
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
-          {
-            key: 'Permissions-Policy',
-            value:
-              'accelerometer=(), autoplay=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: csp.join('; '),
-          },
-        ],
-      },
-    ]),
 }
-
-const csp = [
-  "default-src 'unsafe-inline' 'self'",
-  "img-src 'self' data: https://avatars.githubusercontent.com https://gravatar.com",
-  "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-  "manifest-src 'self'",
-  "connect-src 'self' https://vitals.vercel-insights.com https://o59230.ingest.sentry.io",
-  "style-src-elem 'self' 'unsafe-inline'",
-  "worker-src 'self' blob:",
-]
 
 // Injected content via Sentry wizard below
 
