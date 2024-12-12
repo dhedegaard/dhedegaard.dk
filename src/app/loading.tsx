8e0914f40661a716a32bcbbb43599ed9a4188b60
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-12-12 17:18:53 +0100

    Let's try with a simple loading page, for showing something faster.

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function InboxLoading() {
+  return (
+    <div className="mb-16 mt-8 flex gap-4">
+      <div className="flex flex-auto flex-col gap-6">
+        <h1 className="animate-slideTitle text-5xl">Dennis Hedegaard</h1>
+      </div>
+    </div>
+  )
+}
