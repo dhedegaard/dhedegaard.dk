8127c6b919a3098e3d5aa839e525af1d6f21bf44
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-05-29 11:54:44 +0200

    use instrumentation-client module

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,13 +2,10 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
+import { init } from '@sentry/nextjs'
 
-Sentry.init({
+init({
   dsn: process.env['NEXT_PUBLIC_SENTRY_DSN'] ?? '',
-
-  // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -19,10 +16,12 @@ Sentry.init({
   replaysSessionSampleRate: 0.1,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   ignoreErrors: ['Suspense Exception'],
 })
+
+export { captureRouterTransitionStart as onRouterTransitionStart } from '@sentry/nextjs'
