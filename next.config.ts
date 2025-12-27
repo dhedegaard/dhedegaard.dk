70369f23d20910fa2566b2ea21e9b3ff0ec30f82
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-12-27 14:26:24 +0100

    cleanup on sentry configs, that have now disappeared

--- next.config.ts
+++ next.config.ts
@@ -37,15 +37,6 @@ export default withBundleAnalyzer({
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
     sourcemaps: {
       deleteSourcemapsAfterUpload: true,
     },
