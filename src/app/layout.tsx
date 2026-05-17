4938566a0b0b663b2af2acc8d58daa6dc8546bd6
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-17 13:40:58 +0200

    extracted gravatar URL to const

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,16 @@ import type { ReactNode } from 'react'
 import { preconnect } from 'react-dom'
 
 const metadataBase = new URL('https://www.dhedegaard.dk/')
+const gravatarAvatar =
+  'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f'
+
 export const metadata: Metadata = {
   title: 'Dennis Hedegaard',
   keywords: 'Dennis Hedegaard, Dennis Elsborg Heick Hedegaard, dhedegaard',
   description: 'The personal website of Dennis Hedegaard',
   manifest: '/manifest.json',
-  icons:
-    'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f',
+  icons: gravatarAvatar,
   metadataBase,
-  robots: 'index, follow',
   appleWebApp: {
     capable: true,
     title: 'Dennis Hedegaard',
@@ -26,20 +27,20 @@ export const metadata: Metadata = {
     card: 'summary',
     images: [
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=512',
+        url: `${gravatarAvatar}?s=512`,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=512',
+        url: `${gravatarAvatar}?s=512`,
         width: 512,
         height: 512,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=256',
+        url: `${gravatarAvatar}?s=256`,
         width: 256,
         height: 256,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=128',
+        url: `${gravatarAvatar}?s=128`,
         width: 128,
         height: 128,
       },
@@ -53,20 +54,20 @@ export const metadata: Metadata = {
     description: 'The personal website of Dennis Hedegaard',
     images: [
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=512',
+        url: `${gravatarAvatar}?s=512`,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=512',
+        url: `${gravatarAvatar}?s=512`,
         width: 512,
         height: 512,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=256',
+        url: `${gravatarAvatar}?s=256`,
         width: 256,
         height: 256,
       },
       {
-        url: 'https://gravatar.com/avatar/d3fc3961d888b6792ee5b869bc64094527509d6ee9eb1e60bde5854009eb640f?s=128',
+        url: `${gravatarAvatar}?s=128`,
         width: 128,
         height: 128,
       },
