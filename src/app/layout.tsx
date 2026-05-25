a53b66d731fb7f497b7425a96ce0013027f73d70
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-25 21:37:11 +0200

    use next analytics instead of react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata, Viewport } from 'next'
 import type { ReactNode } from 'react'
